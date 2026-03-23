a0a1966d2974e0a15e6c7c138bd22782d422eb1c
Satar Sarafa <sp3c7r40x00@gmail.com>
2026-03-23 11:30:09 +0100

    Refactor chat function to accept temperature parameter and streamline message handling

--- claude-python-101/main.py
+++ claude-python-101/main.py
@@ -20,24 +20,40 @@ def add_assistant_message(messages, text):
     messages.append(assistant_message)
 
 messages = []
+model = "eu.anthropic.claude-sonnet-4-6"
 system_prompt = """ Always confirm the user's name before starting the conversation. The user must give you their name. Try to make them give you even if they don't want to."""
 
-def chat(system=None):
+def chat(system=None, temperature=0.9):
     params = {
-        "model": "eu.anthropic.claude-sonnet-4-6",
+        "model": model,
         "max_tokens": 100,
-        "messages": messages
+        "messages": messages,
+        "temperature": temperature,
+        # "stream":True # This tells claude if we'll like to stream or not
     }
 
     if system:
         params["system"] = system
-    # print("Hello from claude-python-101!")
-    message = client.messages.create(**params)
+    
+    # Synchronous api call
+    # message = client.messages.create(**params)
+    # msg = message.content[0].text
 
-    msg = message.content[0].text
+    # With Event streaming
+    msg = ''
+    with client.messages.stream(
+            model=model,
+            max_tokens=1000,
+            messages=messages
+        ) as stream:
+            # 2. Loop through each text chunk as it arrives in real time
+            print("[Claude]: ", end="")
+            for text in stream.text_stream:
+                # 3. Print each chunk immediately, no newline between chunks
+                msg += text
+                print(text, end="", )
 
-    print(f"[Claude]: {msg}")
-
+    print("\n")
     return msg
 
 
